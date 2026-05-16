79ac8743b1a1a27467efceb031c5f51eaf7c717d
luizxsantos <lzxsantos11@gmail.com>
2026-05-16 14:51:26 -0300

    feat: factory relatorio generator

---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/generator/RelatorioGeneratorFactory.java
@@ -0,0 +1,16 @@
+package br.unicamp.padroescriacionais.legacy.generator;
+
+import br.unicamp.padroescriacionais.legacy.domain.FormatoRelatorio;
+
+public class RelatorioGeneratorFactory {
+
+    public RelatorioGenerator criarGerador(FormatoRelatorio formato) {
+        return switch (formato) {
+            case PDF -> new PdfRelatorioGenerator();
+            case CSV -> new CsvRelatorioGenerator();
+            case JSON -> new JsonRelatorioGenerator();
+            case XML -> new XmlRelatorioGenerator();
+            case HTML -> new HtmlRelatorioGenerator();
+        };
+    }
+}
