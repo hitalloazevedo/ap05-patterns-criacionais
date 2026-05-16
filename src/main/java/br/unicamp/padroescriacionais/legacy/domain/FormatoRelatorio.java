9b5a743b34bdf798a3b108825e352f8f920bb523
luizxsantos <lzxsantos11@gmail.com>
2026-05-16 14:50:56 -0300

    feat: novos formatos de relatorio

--- src/main/java/br/unicamp/padroescriacionais/legacy/domain/FormatoRelatorio.java
+++ src/main/java/br/unicamp/padroescriacionais/legacy/domain/FormatoRelatorio.java
@@ -3,5 +3,17 @@ package br.unicamp.padroescriacionais.legacy.domain;
 public enum FormatoRelatorio {
     PDF,
     CSV,
-    JSON
+    JSON,
+    XML,
+    HTML;
+
+    public String getExtensao() {
+        return switch (this) {
+            case PDF -> "pdf";
+            case CSV -> "csv";
+            case JSON -> "json";
+            case XML -> "xml";
+            case HTML -> "html";
+        };
+    }
 }
